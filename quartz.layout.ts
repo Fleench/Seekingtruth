d8a633dc7bcaf2de39abfc67df045c7203314c15
Flench04 <47568518+Fleench@users.noreply.github.com>
2025-09-09 09:34:12 -0600

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,7 +24,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-    Component.MobileOnly(Component.CustomProperties()),
+    //Component.MobileOnly(Component.CustomProperties()),
   ],
   left: [
     Component.PageTitle(),
@@ -43,7 +43,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.Graph(),
-    Component.DesktopOnly(Component.CustomProperties()),
+    //Component.DesktopOnly(Component.CustomProperties()),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
